d10368509bfd609c514429c4aaf5465b80c986f3
salamshanto <abdussalamshanto2001@gmail.com>
2024-09-22 12:35:20 +0600

    create todo app

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue';
+
+export default {
+  components: {
+    TodoList
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
---
+++ src/components/Todolist.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo.text }}
+        <button @click="removeTodo(index)">Remove</button>
+      </li>
+    </ul>
+    <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Add a new task" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [
+        { text: 'Learn Vue' },
+        { text: 'Build a Todo App' }
+      ],
+      newTodo: ''
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim()) {
+        this.todos.push({ text: this.newTodo });
+        this.newTodo = '';
+      }
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style scoped>
+input {
+  margin-top: 10px;
+}
+button {
+  margin-left: 10px;
+  color: red;
+  cursor: pointer;
+}
+</style>
